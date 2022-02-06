0e43cd5f3805a85cda133d82b7352379788ceef2
Jonas Bird <91971493+jonas-bird@users.noreply.github.com>
2022-02-06 02:51:25 -0500

    Modify script.js
    Modify script.js so that it prompts the player for input, and keeps
    track of the results over 5 games, currently in a playable condition

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
+// main game function
+function game() {
+  // declare a variable to hold the overall results
+  let wins = 0;
+  let losses = 0;
+  // tell the player what is going on
+  console.log("Lets play rock, paper, scissors, best of 5");
+  // loop through 5 games
+  for (let i = 0; i < 5; i++) {
+    // prompt the user
+    const playerSelection = window.prompt(
+      "Please enter rock, paper or scissors: ",
+      ""
+    );
+    const computerSelection = computerPlay();
+    const result = playRound(playerSelection, computerSelection);
+    console.log(result);
+    // check to see if the player won
+    if (result[4] == "w") {
+      wins++;
+    } else if (result[4] == "l") {
+      losses++;
+    }
+  } // end for loop
+  // print overall results
+  if (wins > losses) {
+    console.log("Good game! You win.");
+  } else if (losses > wins) {
+    console.log("Too bad, you lost. Better luck next time");
+  } else {
+    console.log("Hmm, could have gone worse...");
+  }
+} // end game function
+
 // computer chooses randomly
 function computerPlay() {
   let randChoice = Math.floor(Math.random() * 3);
@@ -9,7 +43,7 @@ function computerPlay() {
     return "scissors";
   }
 }
-
+// compare results and return results of the round
 function playRound(playerSelection, computerSelection) {
   // make sure that the players choice is in lower case
   safePlayerSelection = playerSelection.toLowerCase();
@@ -43,16 +77,4 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-// testing code
-let playerSelection = "rock";
-for (let i = 0; i < 5; i++) {
-  const computerSelection = computerPlay();
-  console.log(playRound(playerSelection, computerSelection));
-}
-// test to make sure that I properly converted payer input to lowercase
-const computerSelection = computerPlay;
-playerSelection = "Rock";
-console.log(playRound(playerSelection, computerSelection));
-// make sure that the script does not crash on bad input
-playerSelection = "Spock";
-console.log(playRound(playerSelection, computerSelection));
+game();
